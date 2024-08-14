888a7da462d1fdfdba7f127d5849bdbbb202d468
Anjali Patel <anjali.140410107052@gmail.com>
2024-08-14 18:18:31 +0530

    Added Payment Method field in invoice.

--- dist/utils/config.js
+++ dist/utils/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var o=(e,t)=>()=>(t||e((t={exports:{}}).exports,t),t.exports);var i=o((T,a)=>{var l={string:{invoice:"I N V O I C E",refNumber:"Ref no",date:"Date",status:"Status",billTo:"Bill To",item:"Item",quantity:"Qty",price:"Price",tax:"Tax",total:"Total",subTotal:"Subtotal",deliveryFee:"Delivery Fee",discountAmount:"Discount Amount",totalTax:"Total Tax"}};a.exports=l;});var config = i();
+var o=(e,t)=>()=>(t||e((t={exports:{}}).exports,t),t.exports);var i=o((T,a)=>{var l={string:{invoice:"I N V O I C E",refNumber:"Ref no",date:"Date",status:"Status",billTo:"Bill To",item:"Item",quantity:"Qty",price:"Price",tax:"Tax",total:"Total",subTotal:"Subtotal",deliveryFee:"Delivery Fee",paymentMethod:"Payment Method",discountAmount:"Discount Amount",totalTax:"Total Tax"}};a.exports=l;});var config = i();
 
 module.exports = config;
 //# sourceMappingURL=out.js.map
