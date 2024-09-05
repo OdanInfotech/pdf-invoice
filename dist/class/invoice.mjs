bc446cad638870a2d27b99c3176206de4de41fdc
Anjali Patel <anjali.140410107052@gmail.com>
2024-09-05 15:23:55 +0530

    code changes

--- dist/class/invoice.mjs
+++ dist/class/invoice.mjs
@@ -15,7 +15,7 @@ var u = (s => typeof require < "u" ? require : typeof Proxy < "u" ? new Proxy(s,
  ${n.quantity}`,`
  ${this.currency}${n.price}`,`
  ${n.tax||0}%`,`
- ${this.currency}${b}`]);}),t.push(m);let x={margin:[0,-100,0,0],columns:[{width:"*",stack:[" "],style:"text"},{width:200,lineHeight:1.5,style:"textBold",table:{widths:[80,"*"],headerRows:1,lineHeight:1.5,body:[[`
+ ${this.currency}${b}`]);}),t.push(m);let x={margin:[0,-120,0,0],columns:[{width:"*",stack:[" "],style:"text"},{width:200,lineHeight:1.5,style:"textBold",table:{widths:[80,"*"],headerRows:1,lineHeight:1.5,body:[[`
  ${this.config.string.subTotal}`,`
  ${this.currency}${r.calcSubTotal(this.items)}`,`
  ${this.currency}${this.invoice.deliveryFee}`],[`
